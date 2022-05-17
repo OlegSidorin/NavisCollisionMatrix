21c573d698ef0ae214dd4c726f88e8556c98661c
Oleg Sidorin <oleg.sidorin@gmail.com>
2022-05-17 20:22:58 +0300

    995646

--- СollisionMatrix/MatrixCreatingViewModel.cs
+++ СollisionMatrix/MatrixCreatingViewModel.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace СollisionMatrix
 {
@@ -12,6 +17,7 @@ namespace СollisionMatrix
     {
         public ObservableCollection<MatrixSelectionLineModel> Selections { get; set; }
         public ObservableCollection<UserControl> UserControlsInWholeMatrix { get; set; }
+        public ObservableCollection<UserControl> UserControlsSelectionNames { get; set; }
         public MatrixCreatingViewModel()
         {
             Selections = new ObservableCollection<MatrixSelectionLineModel>();
@@ -20,7 +26,7 @@ namespace СollisionMatrix
                 NameOfSelection = "AR_Walls",
                 SelectionIntersectionTolerance = new List<string>() 
                 {
-                    "23", "", ""
+                    "15", "", "", ""
                 }
             };
             var new2 = new MatrixSelectionLineModel()
@@ -28,7 +34,7 @@ namespace СollisionMatrix
                 NameOfSelection = "AR_Floors",
                 SelectionIntersectionTolerance = new List<string>()
                 {
-                    "", "12", ""
+                    "", "30", "", ""
                 }
             };
             var new3 = new MatrixSelectionLineModel()
@@ -36,15 +42,25 @@ namespace СollisionMatrix
                 NameOfSelection = "AR_Doors",
                 SelectionIntersectionTolerance = new List<string>()
                 {
-                    "", "", "5"
+                    "", "", "80", ""
+                }
+            };
+            var new4 = new MatrixSelectionLineModel()
+            {
+                NameOfSelection = "AR_Windows",
+                SelectionIntersectionTolerance = new List<string>()
+                {
+                    "", "", "", "50"
                 }
             };
 
             Selections.Add(new1);
             Selections.Add(new2);
             Selections.Add(new3);
+            Selections.Add(new4);
 
             UserControlsInWholeMatrix = new ObservableCollection<UserControl>();
+            UserControlsSelectionNames = new ObservableCollection<UserControl>();
             foreach (MatrixSelectionLineModel model in Selections)
             {
                 MatrixSelectionLineViewModel userControlvm = new MatrixSelectionLineViewModel();
@@ -67,15 +83,152 @@ namespace СollisionMatrix
                     userControlvm.ToleranceViews.Add(cellView);
                 }
 
-
                 MatrixSelectionLineUserControl userControl = new MatrixSelectionLineUserControl();
                 userControlvm.UserControl_MatrixSelectionLineUserControl = userControl;
                 userControlvm.UserControlsInAllMatrixWithLineUserControls = UserControlsInWholeMatrix;
+                userControlvm.UserControlsInSelectionNameUserControls = UserControlsSelectionNames;
                 userControl.DataContext = userControlvm;
                 UserControlsInWholeMatrix.Add(userControl);
             };
-            
+
+            foreach (UserControl usercontrolline in UserControlsInWholeMatrix)
+            {
+                MatrixSelectionLineUserControl msluc = (MatrixSelectionLineUserControl)usercontrolline;
+                MatrixSelectionLineViewModel mslvm = (MatrixSelectionLineViewModel)msluc.DataContext;
+
+                MatrixSelectionNameUserControl msnuc = new MatrixSelectionNameUserControl();
+                msnuc.DataContext = mslvm;
+                UserControlsSelectionNames.Add(msnuc);
+            }
+
+            DoIfIClickOnCreateXMLCollisionMatrixButton = new RelayCommand(OnDoIfIClickOnCreateXMLCollisionMatrixButtonExecuted, CanDoIfIClickOnCreateXMLCollisionMatrixButtonExecute);
+
+            int linecounter = 0;
+            int cellcounter = 0;
+            foreach (UserControl usercontrolline in UserControlsInWholeMatrix)
+            {
+                MatrixSelectionLineUserControl msluc = (MatrixSelectionLineUserControl)usercontrolline;
+                MatrixSelectionLineViewModel mslvm = (MatrixSelectionLineViewModel)msluc.DataContext;
+                cellcounter = 0;
+                foreach (UserControl usercontrolcell in mslvm.ToleranceViews)
+                {
+                    MatrixSelectionCellUserControl mscuc = (MatrixSelectionCellUserControl)usercontrolcell;
+                    MatrixSelectionCellVewModel mscvm = (MatrixSelectionCellVewModel)mscuc.DataContext;
+
+                    cellcounter += 1;
+                }
+                linecounter += 1;
+            }
 
         }
+
+        public ICommand DoIfIClickOnCreateXMLCollisionMatrixButton { get; set; }
+        private void OnDoIfIClickOnCreateXMLCollisionMatrixButtonExecuted(object p)
+        {
+            XmlDocument xDoc = new XmlDocument();
+
+            XmlDeclaration xmlDeclaration = xDoc.CreateXmlDeclaration("1.0", "UTF-8", string.Empty);
+            XmlNode xmlDeclaration_node = xDoc.AppendChild(xmlDeclaration);
+
+
+            XmlElement exchange_element = xDoc.CreateElement("exchange");
+            exchange_element.SetAttribute(@"xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
+            exchange_element.SetAttribute("noNamespaceSchemaLocation", "http://www.w3.org/2001/XMLSchema-instance", "http://download.autodesk.com/us/navisworks/schemas/nw-exchange-12.0.xsd");
+            //exchange_element.SetAttribute("xsi:noNamespaceSchemaLocation", "http://download.autodesk.com/us/navisworks/schemas/nw-exchange-12.0.xsd"); // not working
+            exchange_element.SetAttribute("units", "ft");
+            exchange_element.SetAttribute("filename", "");
+            exchange_element.SetAttribute("filepath", "");
+
+            XmlNode exchange_node = xDoc.AppendChild(exchange_element);
+
+
+            XmlElement batchtest_element = xDoc.CreateElement("batchtest");
+            batchtest_element.SetAttribute("name", "Без имени");
+            batchtest_element.SetAttribute("internal_name", "Без имени");
+            batchtest_element.SetAttribute("units", "ft");
+            XmlNode batchtest_node = exchange_node.AppendChild(batchtest_element);
+
+            XmlElement clashtests_element = xDoc.CreateElement("clashtests");
+            XmlNode clashtests_node = batchtest_node.AppendChild(clashtests_element);
+            int iForToleranceArray = 0;
+            foreach(UserControl usercontrolline in UserControlsInWholeMatrix)
+            {
+                iForToleranceArray = 0;
+                MatrixSelectionLineUserControl msluc = (MatrixSelectionLineUserControl)usercontrolline;
+                MatrixSelectionLineViewModel mslvm = (MatrixSelectionLineViewModel)msluc.DataContext;
+                foreach (UserControl usercontrolcell in mslvm.ToleranceViews)
+                {
+                    MatrixSelectionCellUserControl mscuc = (MatrixSelectionCellUserControl)usercontrolcell;
+                    MatrixSelectionCellVewModel mscvm = (MatrixSelectionCellVewModel)mscuc.DataContext;
+                    if (mscvm.Tolerance != string.Empty)
+                    {
+                        bool result = double.TryParse(mscvm.Tolerance, out double dt);
+                        if (result)
+                        {
+                            MatrixSelectionLineUserControl uci = (MatrixSelectionLineUserControl)UserControlsInWholeMatrix.ElementAt(iForToleranceArray);
+                            MatrixSelectionLineViewModel vmi = (MatrixSelectionLineViewModel)uci.DataContext;
+                            string nameOfOtherSelection = vmi.NameOfSelection;
+                            XmlElement clashtest_element = xDoc.CreateElement("clashtest");
+                            clashtest_element.SetAttribute("name", mslvm.NameOfSelection + " - " + nameOfOtherSelection);
+                            clashtest_element.SetAttribute("test_type", "hard");
+                            clashtest_element.SetAttribute("status", "new");
+                            clashtest_element.SetAttribute("tolerance", (dt / 304.78).ToString().Replace(",", ".").Substring(0,12));
+                            clashtest_element.SetAttribute("merge_composites", "1");
+                            XmlNode clashtest_node = clashtests_node.AppendChild(clashtest_element);
+
+                            // <linkage mode="none"/>
+                            XmlElement linkage_element = xDoc.CreateElement("linkage");
+                            linkage_element.SetAttribute("mode", "none");
+                            XmlNode linkage_node = clashtest_node.AppendChild(linkage_element);
+
+                            // <left>
+                            XmlElement left_element = xDoc.CreateElement("left");
+                            XmlNode left_node = clashtest_node.AppendChild(left_element);
+
+                            // <clashselection selfintersect="0" primtypes="1">
+                            XmlElement clashselection_element = xDoc.CreateElement("clashselection");
+                            clashselection_element.SetAttribute("selfintersect", "0");
+                            clashselection_element.SetAttribute("primtypes", "1");
+                            XmlNode clashselection_node = left_node.AppendChild(clashselection_element);
+
+                            // <locator>lcop_selection_set_tree/АР_Стены</locator>
+                            XmlElement locator_element = xDoc.CreateElement("locator");
+                            locator_element.InnerText = @"lcop_selection_set_tree/" + mslvm.NameOfSelection;
+                            XmlNode locator_node = clashselection_node.AppendChild(locator_element);
+
+                            // <right>
+                            XmlElement right_element = xDoc.CreateElement("right");
+                            XmlNode right_node = clashtest_node.AppendChild(right_element);
+
+                            // <clashselection selfintersect="0" primtypes="1">
+                            XmlElement clashselectionR_element = xDoc.CreateElement("clashselection");
+                            clashselectionR_element.SetAttribute("selfintersect", "0");
+                            clashselectionR_element.SetAttribute("primtypes", "1");
+                            XmlNode clashselectionR_node = right_node.AppendChild(clashselectionR_element);
+
+                            // <locator>lcop_selection_set_tree/АР_Стены</locator>
+                            XmlElement locatorR_element = xDoc.CreateElement("locator");
+                            locatorR_element.InnerText = @"lcop_selection_set_tree/" + nameOfOtherSelection;
+                            XmlNode locatorR_node = clashselectionR_node.AppendChild(locatorR_element);
+
+                            // <rules/>
+                            XmlElement rules_element = xDoc.CreateElement("rules");
+                            XmlNode rules_node = clashtest_node.AppendChild(rules_element);
+                        }
+                    }
+                    iForToleranceArray += 1;
+                }
+
+                
+            }
+
+            XmlElement selectionsets_element = xDoc.CreateElement("selectionsets");
+            XmlNode selectionsets_node = batchtest_node.AppendChild(selectionsets_element);
+
+            xDoc.Save(@"C:\Users\o.sidorin\Downloads\examplexml.xml");
+
+        }
+        private bool CanDoIfIClickOnCreateXMLCollisionMatrixButtonExecute(object p) => true;
+
     }
 }
--- СollisionMatrix/MatrixSelectionLineViewModel.cs
+++ СollisionMatrix/MatrixSelectionLineViewModel.cs
@@ -54,8 +54,8 @@ namespace СollisionMatrix
             }
         }
 
-        public ICommand DoIfButtonDeletePushCommand { get; set; }
-        private void OnDoIfButtonDeletePushCommandExecuted(object p)
+        public ICommand DoIfIClickDeleteButton { get; set; }
+        private void OnDoIfIClickDeleteButtonExecuted(object p)
         {
             //MessageBox.Show($"{RowNum} from {Selections.Count} and {Selection.NameOfSelection}");
             // Удалить ячейку с номером строки из всех строк из модели Пересечений Selections
@@ -68,20 +68,84 @@ namespace СollisionMatrix
             {
                 MatrixSelectionLineUserControl ucl = (MatrixSelectionLineUserControl)uc;
                 MatrixSelectionLineViewModel uclvm = (MatrixSelectionLineViewModel)ucl.DataContext;
-                uclvm.ToleranceViews.Remove(uclvm.ToleranceViews.ElementAt(RowNum));
+                if (RowNum < uclvm.ToleranceViews.Count()) uclvm.ToleranceViews.Remove(uclvm.ToleranceViews.ElementAt(RowNum));
                 if (uclvm.RowNum > RowNum) uclvm.RowNum -= 1; 
             }
-            Selections.Remove(Selection); // удалить из Общего списка Пересечений Selections из модели, которая будет использована для экспорта в xml в итоге
+            //Selections.Remove(Selection); // удалить из Общего списка Пересечений Selections из модели, которая будет использована для экспорта в xml в итоге
             UserControlsInAllMatrixWithLineUserControls.Remove(UserControl_MatrixSelectionLineUserControl); // Удалить из списка всех вью-строчек на окне
+            MatrixSelectionNameUserControl userControlForDeleting = null;
+            foreach (var uc in UserControlsInSelectionNameUserControls)
+            {
+                MatrixSelectionNameUserControl msnuc = (MatrixSelectionNameUserControl)uc;
+                MatrixSelectionLineViewModel uclvm = (MatrixSelectionLineViewModel)msnuc.DataContext;
+                if (uclvm.NameOfSelection == NameOfSelection) userControlForDeleting = msnuc;
+            }
+            if (userControlForDeleting != null) UserControlsInSelectionNameUserControls.Remove(userControlForDeleting);
+        }
+        private bool CanDoIfIClickDeleteButtonExecute(object p) => true;
 
+        public ICommand DoIfIClickBottomAddButton { get; set; }
+        private void OnDoIfIClickBottomAddButtonExecuted(object p)
+        {
+            // creating new view user control line
+            int indexOfNewRow = RowNum + 1;
+            MatrixSelectionLineViewModel lineViewModel_new = new MatrixSelectionLineViewModel();
+            lineViewModel_new.RowNum = indexOfNewRow;
+            lineViewModel_new.NameOfSelection = "XX_New";
             
+            lineViewModel_new.ToleranceViews = new ObservableCollection<UserControl>();
+            for (int i = 0; i < UserControlsInAllMatrixWithLineUserControls.Count(); i++)
+            {
+                MatrixSelectionCellVewModel cell_vm = new MatrixSelectionCellVewModel();
+                cell_vm.Tolerance = string.Empty;
+                MatrixSelectionCellUserControl cell_uc = new MatrixSelectionCellUserControl();
+                cell_uc.DataContext = cell_vm;
+                lineViewModel_new.ToleranceViews.Add(cell_uc);
+            }
+            lineViewModel_new.ButtonsVisibility = Visibility.Hidden;
+            lineViewModel_new.UserControlsInAllMatrixWithLineUserControls = UserControlsInAllMatrixWithLineUserControls;
+            lineViewModel_new.UserControlsInSelectionNameUserControls = UserControlsInSelectionNameUserControls;
+
+            MatrixSelectionLineUserControl lineUserControl_new = new MatrixSelectionLineUserControl();
+            lineViewModel_new.UserControl_MatrixSelectionLineUserControl = lineUserControl_new;
+            lineUserControl_new.DataContext = lineViewModel_new;
+
+            MatrixSelectionNameUserControl selectionNameUserControl_new = new MatrixSelectionNameUserControl();
+            selectionNameUserControl_new.DataContext = lineViewModel_new;
+
+            UserControlsInAllMatrixWithLineUserControls.Insert(indexOfNewRow, lineUserControl_new);
+            UserControlsInSelectionNameUserControls.Insert(indexOfNewRow, selectionNameUserControl_new);
+
+            int iterator = 0;
+            foreach (UserControl usercontrolline in UserControlsInAllMatrixWithLineUserControls)
+            {
+                MatrixSelectionLineUserControl msluc = (MatrixSelectionLineUserControl)usercontrolline;
+                MatrixSelectionLineViewModel mslvm = (MatrixSelectionLineViewModel)msluc.DataContext; // line
+
+                mslvm.RowNum = iterator;
+
+                MatrixSelectionCellVewModel cellViewModel_new = new MatrixSelectionCellVewModel();
+                cellViewModel_new.Tolerance = string.Empty;
+                MatrixSelectionCellUserControl cellUserControl_new = new MatrixSelectionCellUserControl();
+                cellUserControl_new.DataContext = cellViewModel_new;
+
+                mslvm.ToleranceViews.Insert(RowNum + 1, cellUserControl_new);
+
+                foreach (UserControl usercontrolcell in mslvm.ToleranceViews)
+                {
+                    MatrixSelectionCellUserControl mscuc = (MatrixSelectionCellUserControl)usercontrolcell;
+                    MatrixSelectionCellVewModel mscvm = (MatrixSelectionCellVewModel)mscuc.DataContext; // cell
+                }
+                iterator += 1;
+            }
 
         }
-        private bool CanDoIfButtonDeletePushCommandExecute(object p) => true;
+        private bool CanDoIfIClickBottomAddButtonExecute(object p) => true;
 
         public ObservableCollection<MatrixSelectionLineModel> Selections { get; set; }
         public MatrixSelectionLineModel Selection { get; set; }
         public ObservableCollection<UserControl> UserControlsInAllMatrixWithLineUserControls { get; set; }
+        public ObservableCollection<UserControl> UserControlsInSelectionNameUserControls { get; set; }
         public MatrixSelectionLineUserControl UserControl_MatrixSelectionLineUserControl { get; set; }
 
         public ObservableCollection<UserControl> ToleranceViews { get; set; }
@@ -112,7 +176,8 @@ namespace СollisionMatrix
 
             ButtonsVisibility = Visibility.Hidden;
 
-            DoIfButtonDeletePushCommand = new RelayCommand(OnDoIfButtonDeletePushCommandExecuted, CanDoIfButtonDeletePushCommandExecute); 
+            DoIfIClickDeleteButton = new RelayCommand(OnDoIfIClickDeleteButtonExecuted, CanDoIfIClickDeleteButtonExecute);
+            DoIfIClickBottomAddButton = new RelayCommand(OnDoIfIClickBottomAddButtonExecuted, CanDoIfIClickBottomAddButtonExecute); 
         }
     }
 }
